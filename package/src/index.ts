89d251e3bff2c9a6c7b56f02cac6434aa6a75330
Henry Paulino <me@henrypl.com>
2025-12-26 19:05:33 -0500

    model info (#11)

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,6 +1,13 @@
 export { LLM, type Message } from './llm'
 export { ModelManager } from './modelManager'
-export { MLXModel } from './models'
+export {
+  MLXModel,
+  MLXModels,
+  ModelFamily,
+  type ModelInfo,
+  ModelProvider,
+  type ModelQuantization,
+} from './models'
 
 export type { GenerationStats, LLM as LLMSpec, LLMLoadOptions } from './specs/LLM.nitro'
 export type { ModelManager as ModelManagerSpec } from './specs/ModelManager.nitro'
